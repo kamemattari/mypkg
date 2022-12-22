ded4ef9db0270612f3a9e89280a9d1450a33c65c
A1ka <s21c1033ar@s.chubakoudai.jp>
2022-12-23 04:13:17 +0900

    fix

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,10 +1,12 @@
+# SPDX-FileCopyrightTest: 2022 Aika katsuki
+# SPDX-License-Identifier: BSD-3-Clause
 import rclpy
 from rclpy.node import Node
 from person_msg.msg import Person
 
 def cb(msg):
     global node
-    node.get_logger().info("Listen: %s"% msg)
+    node.get_logger().info("Listen: %s"% msg.data)
 
 rclpy.init()
 node = Node("listener")
